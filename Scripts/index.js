39f58fac496ae0a9ffe11b591ebecc3d431e9018
Swaraj Trivedi <swaraj.t@lsq.net>
2022-02-02 00:48:59 +0530

    first

---
+++ Scripts/index.js
@@ -0,0 +1,21 @@
+var index=0;
+var imageList = [
+    './Assets/images/A1.jpg',
+    './Assets/images/A2.jpg',
+    './Assets/images/A3.jpg',
+    './Assets/images/A4.jpg',
+    './Assets/images/A5.jpg'
+];
+function goLeft (){
+    if(index>0){
+        index-=1;
+        document.getElementById("parent").src=imageList[index];
+    }
+}
+function goRight (){
+    if(index<4){
+        index++;
+        document.getElementById("parent").src =imageList[index];
+    }
+}
+setInterval(goRight,1000);
